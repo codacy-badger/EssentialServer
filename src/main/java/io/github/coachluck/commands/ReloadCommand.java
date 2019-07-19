0a7cc7fe7f348cdf5cd98cd825401b5e770baaaa
ajrom47 <beastlyaj01@gmail.com>
2019-07-19 04:33:31 -0400

    -

--- src/main/java/io/github/coachluck/commands/ReloadCommand.java
+++ src/main/java/io/github/coachluck/commands/ReloadCommand.java
@@ -1,10 +1,10 @@
 package io.github.coachluck.commands;
 
 import io.github.coachluck.EssentialServer;
-import io.github.coachluck.files.MotdConfig;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
 
 import static io.github.coachluck.utils.ChatUtils.format;
 import static io.github.coachluck.utils.ChatUtils.msg;
@@ -18,7 +18,7 @@ public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
 
-            MotdConfig.reload();
+        //add support for motd file
             plugin.reloadConfig();
             msg(sender, format("&aConfiguration files have been reloaded!"));
         return true;
