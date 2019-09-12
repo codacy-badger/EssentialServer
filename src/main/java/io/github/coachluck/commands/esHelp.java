bb78ea80ccb67528f1f9e30d58d66d40a80ff2c2
CoachLuck <beastlyaj01@gmail.com>
2019-09-11 18:41:32 -0600

    added help

--- src/main/java/io/github/coachluck/commands/esHelp.java
+++ src/main/java/io/github/coachluck/commands/esHelp.java
@@ -4,6 +4,7 @@ import io.github.coachluck.EssentialServer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import static io.github.coachluck.utils.ChatUtils.*;
 
 import java.util.ArrayList;
 
@@ -14,28 +15,34 @@ public class esHelp implements CommandExecutor {
     }
 
     private final int PAGE_LENGTH = 6;
-    private int pages;
+    private int pages = 2;
 
     private String[] commands = {
-            "Help: Get help about commands.",
-            "Whisper: Send a private message to another player.",
-            "Reply: Reply to a private message from another player.",
-            "Money: Check how much money you currently have.",
-            "command1: description1",
-            "command2: description2",
-            "command3: description3",
-            "command4: description4",
+
+            "&a/&cesHelp <&b#&c>&7: &aGet help about commands. &c<>&a is optional.",
+            "&a/&csetSpawn&7: &aSets the spawn for the server.",
+            "&a/&cSpawn <&bP&c>&7: &aTeleports you or the player to spawn.",
+            "&a/&cClear <&bP&c>&7: &aClears you or the players inventory.",
+            "           &a<&c/ci&a, /clearinv&a, /clearinventory&a>",
+            "&a/&cFeed <&bP&c>&7: &aRestores your or the players hunger.",
+            "&a/&cHeal <&bP&c>&7: &aHeal yourself or the player.",
+            "&a/&cGod <&bP&c>&7: &aMakes your completely invincible.",
+            "&a/&cKill <&bP&c>&7: &aKill yourself or the player.",
+            "&a/&cSmite <&bP&c>&7: &aThor is watching... <&c/thor&a,&c /lightning&a>.",
+            "&a/&cBurn <&bP&c>&7: &aBurn yourself or the specified player.",
+            "&a/&cFly <&bP&c>&7: &aMakes you or the named player take flight!",
+            "&a/&cVanish <&bP&c>&7: &aMakes you or the named player disappear!",
     };
 
     public void sendHelp(CommandSender sender, int page) {
 
-        ArrayList<String> message = new ArrayList<String>();
+        ArrayList<String> message = new ArrayList<>();
         message.add("");
-        message.add(" §9-==============-  §lHelp (" + (page+1) + "/" + pages + ")  §9-==============- ");
+        message.add(format(" &9-===============- &6&lHelp &r&7(&6&l" + (page+1) + "&r&7/&6&l" + pages + "&r&7) &9-===============- "));
 
         int start = page * PAGE_LENGTH;
         int end = start + PAGE_LENGTH;
-        for (int i = start; i < end; i++) if (i < commands.length) message.add("§9" + commands[i]);
+        for (int i = start; i < end; i++) if (i < commands.length) message.add(format(" &9|| &r" + commands[i]));
 
         sender.sendMessage(message.toArray(new String[message.size()]));
 
@@ -43,7 +50,7 @@ public class esHelp implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
-        if (cmd.getName().equalsIgnoreCase("help")) {
+        if (cmd.getName().equalsIgnoreCase("eshelp")) {
 
             int i = 0;
             if (args.length != 0) {
@@ -57,7 +64,7 @@ public class esHelp implements CommandExecutor {
 
         }
 
-        return false;
+        return true;
 
     }
 }
